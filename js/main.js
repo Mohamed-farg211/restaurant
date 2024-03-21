339c4bbc05bd249ffb1f86fd4a8851573fa6685b
mhmd_farag <157736566+Mohamed-farg211@users.noreply.github.com>
2024-03-21 07:48:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// var x = document.getElementById("head");
+// var btn = document.getElementById("up");
+
+// window.onscroll = function () {
+//   if (window.scrollY > 200) {
+//     x.classList.add("headadd");
+//     btn.classList.add("btn-f");
+//   } else {
+//     x.classList.remove("headadd");
+//     btn.classList.remove("btn-f");
+//   }
+// };
+
+// btn.addEventListener("click", function () {
+//   window.scroll({
+//     top: 0,
+//     behavior: "smooth",  });
+// });
+
+
+
+
+
+// function int(){
+//     let x=10
+//     console.log(x);
+// }
+// int()
+
+
+
+
+// (
+// function(){
+//     let x=10;
+//     console.log(x);
+// })()
+
+
+
+//  
